5a19c1b6c87df509ecac8a76e3d4359d4ef81f9a
BIT <BIT@DESKTOP-L7VEOSK>
2024-04-23 17:22:44 +0900

    test

--- practice01/src/main/java/prob3/Prob3.java
+++ practice01/src/main/java/prob3/Prob3.java
@@ -7,7 +7,7 @@ public class Prob3 {
 	public static void main(String[] args) {
 
 		Scanner scanner = new Scanner(System.in);
-		
+		//next integer로 변수 받기 
 		/* 코드 작성 */
 		System.out.println("확인");
 		
