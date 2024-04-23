cbba54ccac31113cb61bcbd715e87bc8a684ac50
BIT <BIT@DESKTOP-L7VEOSK>
2024-04-23 21:23:31 +0900

    과제

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -7,7 +7,16 @@ public class Prob1 {
 		Scanner scanner = new Scanner( System.in );
 		
 		/* 코드 작성 */
+		System.out.printf("수를 입력하시오 : ");
+		int num = scanner.nextInt();
 		
+		System.out.println(num);
+		//if num %3 ==0:
+		//	print('3의 배수입니다.')
+		
+		if(num%3==0) {
+			System.out.println("3의 배수입니다.");
+		}
 		scanner.close();
 	}
 }
