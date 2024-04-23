4bfd95480205486d3b4c1375c594b3d4b053e3a7
BIT <BIT@DESKTOP-L7VEOSK>
2024-04-23 21:51:46 +0900

    과제 변경

--- practice01/src/main/java/prob5/Prob5.java
+++ practice01/src/main/java/prob5/Prob5.java
@@ -3,29 +3,25 @@ package prob5;
 import java.util.Scanner;
 
 public class Prob5 {
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner( System.in );
-		int num = scanner.nextInt();
-		String text = Integer.toString(num);
-//		String s = Integer.toString(scanner.nextInt());
-//		System.out.println(s);
-		
-		int cnt = 0;
-		for(int i=0; i<text.length();i++) {
-//			System.out.println(text.charAt(i));
-			char c = text.charAt(i);
-//			num = Integer.parseInt(text.charAt(i));
-			if(c=='3' ||c == '6' ||c =='9') {
-				cnt++;
+	public static void main(String[] args) {	
+		for(int i =1; i<100; i++) {
+			String text = Integer.toString(i);
+			int cnt = 0;
+			for(int j=0; j<text.length();j++) {
+				char c = text.charAt(j);
+				if(c=='3' ||c == '6' ||c =='9') {
+					cnt++;
+				}
+			}
+			if(cnt!=0) {
+				System.out.printf(text+ " ");
+			
+			for(int no = 0; no<cnt; no++) {
+				System.out.print("짝");
+				
+			}
+			System.out.println();
 			}
 		}
-		System.out.printf(text+ " ");
-		for(int i = 0; i<cnt; i++) {
-			System.out.print("짝");
-		}
-		
-		
-		
 	}
 }
