ccf0fb8adcaf78ff7a2e48189e35248327f82c7f
BITCOM <BITCOM@BIT>
2024-04-24 23:29:23 +0900

    4/24 과제 업로드

--- practice02/src/main/java/prob04/Prob04.java
+++ practice02/src/main/java/prob04/Prob04.java
@@ -9,15 +9,26 @@ public class Prob04 {
 		printCharArray( c2 );
 	}
 	
-	public static char[] reverse(String str) {
+	public static char[] reverse(String str) {  // "Hello World"
 		/* 코드를 완성합니다 */
+		char[] cs = str.toCharArray();	// string의 각 char의 값을 배열로 저장
+		int n = cs.length;
+		n--;
+		for(int i=n; i>=0; i--) {
+			cs[n-i] = str.charAt(i);  // str.charAt: 저장된 위치의 문자를 반환
+		}
 		
+//		for(char i:cs) {
+//			System.out.printf("%c", i);
+//		}
 		
-		return null;
+		return cs;
 	}
 
 	public static void printCharArray(char[] array){
 		/* 코드를 완성합니다 */
-		System.out.println( array );
+		
+		System.out.println(array);
+		
 	}
 }
