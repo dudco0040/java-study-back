be6430ed5cedaa6a7134423858f45c4a5be2ece7
BIT <BIT@DESKTOP-L7VEOSK>
2024-04-25 10:52:56 +0900

    4/25 1-2교시 실습

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -4,9 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		Point point = new Point();
-		point.setX(10);
-		point.setY(20);
+//		point.setX(10);
+//		point.setY(20);
 		drawPoint(point);
+//		point.disappear(); // 지우는 메소드를 따로 만들지 말고
+		point.show(false);  //true/false - 두 가지 기능을 파라미터로 사용
 	}
 	
 	private static void drawPoint(Point point) {
--- chapter03/src/main/java/paint/Point.java
+++ chapter03/src/main/java/paint/Point.java
@@ -19,9 +19,23 @@ public class Point {
 	public void setY(int y) {
 		this.y = y;
 	}
-
-	public void show() {
-		System.out.println("점(" + x +"," + y + ")");
+	
+	public void show(boolean visible) {
+		if(visible) {  //true
+//			System.out.println("점(" + x +"," + y + ")을 그렸습니다.");	 
+			// 아래의 show() 메소드가 존재하고, 기존에 있는 메소드가 있다면 활용해야 나중에 변경 및 수정에 용이하다. 
+			show();
+		}else {		//false
+//			System.out.println("점(" + x +"," + y + ")을 지웠습니다.");
+			disappear();
+		}
 	}
 
+	public void show() {
+		System.out.println("점(" + x +"," + y + ")을 그렸습니다.");
+	}
+
+	public void disappear() {
+		System.out.println("점(" + x +"," + y + ")을 지웠습니다.");
+	}
 }
