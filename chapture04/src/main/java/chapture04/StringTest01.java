caff71d0f664bc57d7e401b652c85f4a8d912b3b
BITCOM <BITCOM@BIT>
2024-04-26 15:33:17 +0900

    4/26 6교시 실습 업로드

---
+++ chapture04/src/main/java/chapture04/StringTest01.java
@@ -0,0 +1,22 @@
+package chapture04;
+
+public class StringTest01 {
+
+	public static void main(String[] args) {
+		// c:\temp
+		System.out.println("c:\temp");		//c:	emp
+		System.out.println("c:\\temp");		//c:\temp
+		
+		//"Hello"
+		System.out.println("\"hello\"");	//"hello"
+		
+		
+		// 제어문자
+		// \t: tab
+		// \r: carriage return
+		// \n: newline
+		// \b: beep
+
+	}
+
+}
