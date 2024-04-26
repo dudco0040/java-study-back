ff4443c032b7b44aa38a694f1926e799d47bfd42
BITCOM <BITCOM@BIT>
2024-04-27 00:53:37 +0900

    4/25 과제 ing

--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -6,20 +6,40 @@ public class GoodsApp {
 	private static final int COUNT_GOODS = 3;
 
 	public static void main(String[] args) {
+		
 		Scanner scanner = new Scanner(System.in);
 
-		Goods[] goods = new Goods[COUNT_GOODS];
-
+		Goods[] goods = new Goods[COUNT_GOODS];		// 입력 받는 개수의 크기의 배열 생성 - 제품, 가격, 개수 - 배열 X
+		//Goos 클래스의 배열 
 		// 상품 입력
+		// 3개의 객체를 입력받을 것 - 
+		for(int i = 0; i< COUNT_GOODS; i++) {
+			String input = scanner.nextLine();  	// 형식: 제품명 가격 수량
+			String[] inputArr = input.split(" ");	// 배열에 저장
+			
+			String name = inputArr[0];
+			int price = Integer.parseInt(inputArr[1]);
+			int count = Integer.parseInt(inputArr[2]);
+			
+			Goods g = new Goods(); //실제로 선언을 해줌
+			// set 값을 업데이트
+			g.setName(name);
+			g.setPrice(price);
+			g.setCount(count);
+			
+			goods[i] = g;
+			System.out.println(goods);
+		}
 
 		// 상품 출력
-		String line = sacnner.nextLine();
-		// 한번만 받고 /3개로 분리해서 받음
-		String[] data = line.split(" ");
+//		for(i = 0; i<inputArr.length(); i++) {
+//			
+//		}  for 문이 끝남과 동시에 inputArr 변수에 있는 내용은 쓸 수 없음
 		
-		String name = data[0];
-		int price = Integer.parseInt(data[1]);
-		int price = Integer.parseInt(data[2]);
+		for (Goods g:goods) {
+			
+			System.out.println(g.getName() + "(가격:" + g.getPrice() + ")이 " + g.getCount() + "개 입고 되었습니다.");
+		}
 		
 		// 자원정리
 		scanner.close();
