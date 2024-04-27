043edad024258349e619866575c6ac2a7b9a1096
BITCOM <BITCOM@BIT>
2024-04-27 17:04:20 +0900

    4/25 과제 업로드

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -1,10 +1,45 @@
 package prob05;
 
 public class Account {
+	private String accountno;	// 계좌번호
+	private int balance;		// 잔고
+	
+	// 생성자(Constructor)
+	Account(String accountNo){
+		this.setAccountNo(accountNo);
+		System.out.println("계좌가 계설되었습니다.");
+	}
+	
+	// getter & setter
+	public void setAccountNo(String accountNo) {
+		this.accountno = accountNo;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	public String getAccountNo() {
+		return accountno;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	
+	
+	//입금
+	public void save(int money) {
+		balance = balance + money;
+		System.out.println(money+ "원이 입급되었습니다.");
+	}
+	//출금
+	public void deposit(int money) {
+		balance = balance - money; 
+		System.out.println(money + "원이 출금되었습니다.");
+	}
+	
 }
 
 
-// Account class를 만들기
+// Account class를 완성하기
 // 생성자 만들어서 세팅
 // 출력이 가능해야함
-// 
--- practice03/src/main/java/prob05/AccountTest.java
+++ practice03/src/main/java/prob05/AccountTest.java
@@ -4,13 +4,13 @@ public class AccountTest {
 
 	public static void main(String[] args) {
 		
-		Account account = new Account( "078-3762-293" );
+		Account account = new Account( "078-3762-293" );  // 생성과 동시에 값을 대입
 		System.out.println( account.getAccountNo() + "계좌의 잔고는 " + account.getBalance() + "만원입니다." );
 		
 		account.save( 100 ); // 100만원이 입금되었다. 
 		System.out.println( account.getAccountNo() + "계좌의 잔고는 " + account.getBalance() + "만원입니다." );
 
-		account.deposit( 30 ); 
+		account.deposit( 30 ); // 30만원이 출급되었다.
 		System.out.println( account.getAccountNo() + "계좌의 잔고는 " + account.getBalance() + "만원입니다." );
 
 	}
