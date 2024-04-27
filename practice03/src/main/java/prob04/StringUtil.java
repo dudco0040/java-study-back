f453bd4a0d275a2a477c03dd8d7eef300fdf1769
BITCOM <BITCOM@BIT>
2024-04-27 16:35:51 +0900

    4/25 과제 (Method 추가)

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -3,6 +3,8 @@ package prob04;
 public class StringUtil {
 	public static String concatenate(String[] str) {	// 배열 타입의 파라미터 받기
 		// 문자열을 결합하여 리턴하는 메소드 구현
+		
+		// 문자열 사용(concatenate)
 		String result = "";
 		
 		for(int i = 0; i< str.length; i++){
@@ -10,9 +12,16 @@ public class StringUtil {
 			
 		}
 		
-		return result;
+		// StringBuffer 사용(append)
+		StringBuffer sb = new StringBuffer();
+		for(int i = 0; i < str.length; i++) {
+			sb.append(str[i]);
+		}
+		String result2 = sb.toString();
+		
+		
+		return result2; // result1 OR result2
 	}
 }
-
 
 //concatenate 메소드 완성하기
