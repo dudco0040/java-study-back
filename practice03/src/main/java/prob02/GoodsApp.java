2daee820ca6de5f22b2bdddc2bec2a543ef0288f
BITCOM <BITCOM@BIT>
2024-04-27 15:32:52 +0900

    4/25 과제 업로드

--- practice03/src/main/java/prob02/GoodsApp.java
+++ practice03/src/main/java/prob02/GoodsApp.java
@@ -9,33 +9,27 @@ public class GoodsApp {
 		
 		Scanner scanner = new Scanner(System.in);
 
-		Goods[] goods = new Goods[COUNT_GOODS];		// 입력 받는 개수의 크기의 배열 생성 - 제품, 가격, 개수 - 배열 X
-		//Goos 클래스의 배열 
-		// 상품 입력
-		// 3개의 객체를 입력받을 것 - 
+		Goods[] goods = new Goods[COUNT_GOODS];		// Goods 클래스의 배열 - COUNT_GOOS의 크기만큼 공간을 미련
+		// 상품 입력 
 		for(int i = 0; i< COUNT_GOODS; i++) {
 			String input = scanner.nextLine();  	// 형식: 제품명 가격 수량
-			String[] inputArr = input.split(" ");	// 배열에 저장
 			
+			String[] inputArr = input.split(" ");	// 각 값을 배열에 저장
 			String name = inputArr[0];
 			int price = Integer.parseInt(inputArr[1]);
 			int count = Integer.parseInt(inputArr[2]);
 			
-			Goods g = new Goods(); //실제로 선언을 해줌
-			// set 값을 업데이트
+			Goods g = new Goods(); 		//실제로 객체를 선언을 해줌
+			// 메소드 호출해서 set 값을 업데이트
 			g.setName(name);
 			g.setPrice(price);
 			g.setCount(count);
 			
-			goods[i] = g;
-			System.out.println(goods);
+			goods[i] = g;		// 배열에 각 클래스 결과를 대입
+			// System.out.println(goods);
 		}
 
 		// 상품 출력
-//		for(i = 0; i<inputArr.length(); i++) {
-//			
-//		}  for 문이 끝남과 동시에 inputArr 변수에 있는 내용은 쓸 수 없음
-		
 		for (Goods g:goods) {
 			
 			System.out.println(g.getName() + "(가격:" + g.getPrice() + ")이 " + g.getCount() + "개 입고 되었습니다.");
@@ -47,6 +41,4 @@ public class GoodsApp {
 }
 
 //Goods를 구현하는 문제 
-//count
-//reference array?
-//
+//reference array
