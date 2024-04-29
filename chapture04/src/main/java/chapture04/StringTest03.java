d9ede36168b88fb9fe1fb85beb0aa7c47a0d40ef
BITCOM <BITCOM@BIT>
2024-04-29 09:53:26 +0900

    4/29 1교시 실습 업로드

--- chapture04/src/main/java/chapture04/StringTest03.java
+++ chapture04/src/main/java/chapture04/StringTest03.java
@@ -3,6 +3,16 @@ package chapture04;
 public class StringTest03 {
 	
 	public static void main(String[] args) {
+		
+		// String Builder
+//		String s1 = "Hello" + "World" + "java" + 17;
+//		new StringBuilder("Hello")
+//			.append("World")
+//			.append(" java")
+//			.append(17)
+//			.toString();
+		
+		// String Buffer
 		String s1 = "Hello" + "World" + "java" + 17;	//  이게 왜 되는가?
 		new StringBuffer("Hello")
 			.append("World")
@@ -11,8 +21,25 @@ public class StringTest03 {
 			.toString();
 		
 		System.out.println(s1);
+		
+		
+		String s2 = "";
+		for(int i = 0; i<1000000; i++) {
+//			s2 = s2 + "h";
+//			s2 = new StringBuffer(s2).append("h").toString();
+			// 두 코드는 같음
+			// 속도가 매우 느림
+		}
+		
+		StringBuffer sb = new StringBuffer("");
+		for(int i = 0; i<1000000; i++) {
+			sb.append("h");
+		}
+		String s3 = sb.toString();  // 반복문의 수가 클 경우, 이 방법을 사용하는 것이 더 빠르다. 
 	}
 }
 
 // 주로 1번째처럼 사용하지만,
-// 2번쨰 경우처럼 명시적으로 사용해야 할 때가 있음
+// 2번쨰 경우처럼 명시적으로 사용해야 할 때가 있음 (4/26)
+
+// String Builder, String Buffer 결과가 같음 (4/29)
