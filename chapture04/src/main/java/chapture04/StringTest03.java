0c37b6524d8a53748596748c6ba6ddbd3c4ab4e6
BITCOM <BITCOM@BIT>
2024-04-29 10:41:17 +0900

    4/29 2교시 실습 업로드(String Method 추가)

--- chapture04/src/main/java/chapture04/StringTest03.java
+++ chapture04/src/main/java/chapture04/StringTest03.java
@@ -36,7 +36,37 @@ public class StringTest03 {
 			sb.append("h");
 		}
 		String s3 = sb.toString();  // 반복문의 수가 클 경우, 이 방법을 사용하는 것이 더 빠르다. 
+		
+		
+		// String methods..
+		String s4 = "aBcABCabcAbc";
+		System.out.println(s4.length());
+		System.out.println(s4.charAt(2));	// 2번째 문자 찾기
+		System.out.println(s4.indexOf("abxc"));   // 0부터 찾기
+		System.out.println(s4.indexOf("abxc", 7));  // 7부터 찾기
+		System.out.println(s4.substring(3));
+		System.out.println(s4.substring(3, 5));  // 3~4까지 추출
+		
+		String s5 = "	ab	cd	";
+		String s6 = "efg,hij,klm,nop,qrs";
+		
+		String s7 = s5.concat(s6);  // 문자열 합치기
+		System.out.println(s7);
+		System.out.println(s5.trim());  // trim(): 처음과 끝의 space 없애기
+		System.out.println(s5.replaceAll(" ", ""));  // space 공백으로 대체(all)  
+		
+		
+		//tokens
+		String[] tokens = s6.split(",");
+		for(String s: tokens) {
+				System.out.println(s);
+		}
+		String[] tokens2 = s6.split(",");
+		for(String s: tokens2) {
+				System.out.println(s);
+		}
 	}
+	
 }
 
 // 주로 1번째처럼 사용하지만,
