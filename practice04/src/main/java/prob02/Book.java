9f0115a6167d2f0c16f95aecce3cafa426e99c70
BITCOM <BITCOM@BIT>
2024-04-29 18:16:59 +0900

    4/26 과제 업로드(주석추가)

--- practice04/src/main/java/prob02/Book.java
+++ practice04/src/main/java/prob02/Book.java
@@ -4,7 +4,7 @@ public class Book {
 	int bookNo;		 // 번호
 	String title; 	 // 제목
 	String author; 	 // 작가
-	int stateCode; 	 // 상태코드 - defalt: 0, 호풀 시 1로 업데이트
+	int stateCode; 	 // 상태코드 - defalt: 0, 호출 시 1로 업데이트
 	
 	// 생성자
 	public Book(int bookNo, String title, String authuor) {
@@ -56,13 +56,7 @@ public class Book {
 	
 	// print
 	public void print() {
-//		// stateCode가 1이면 "재고있음" 0이면 "대여중"을 출력
-//		if(stateCode == 1) {
-//			System.out.println("재고 있음");  //return "재고 있음";
-//		} else if(stateCode == 0) {
-//			System.out.println("대여 중");	//return "대여 중"; 
-//		}
-		
+		// 책의 정보 출력
 		System.out.println("[" + bookNo + "] 책 제목:" + title + ", 작가:" + author + ", 대여 유무:" + (stateCode == 1 ? "재고있음" : "대여중"));
 		}
 }
--- practice04/src/main/java/prob02/BookShop.java
+++ practice04/src/main/java/prob02/BookShop.java
@@ -26,7 +26,7 @@ public class BookShop {
 		// (1) 입력된 번호에 맞는 책을 찾아 대여 되었음(상태코드=0)을 체크 합니다.
 		// rent 메소드 호출
 		for (int i=0; i<books.length; i++) {
-			if(books[i].getBookNo() == num) {
+			if(books[i].getBookNo() == num) {  //books[i].method / books[i] => new Book
 				books[i].rent();  // 빌려감과 동시에 0으로 업데이트
 			}
 		}
