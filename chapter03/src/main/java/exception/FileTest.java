dccd0bb3125004e158df6b41c4dc69766213f1f0
BITCOM <BITCOM@BIT>
2024-04-30 19:37:34 +0900

    4/26 2교시 실습(Checked Error) - 주석 추가

--- chapter03/src/main/java/exception/FileTest.java
+++ chapter03/src/main/java/exception/FileTest.java
@@ -9,24 +9,26 @@ public class FileTest {
 	public static void main(String[] args) {
 		FileInputStream fis = null;
 		
+		// try-catch 예외처리
 		try {
 			fis = new FileInputStream("hello.txt");  // 파일이 존재하지 않을 경우, 에러 발생
 			int data = fis.read();
 			System.out.println((char)data);
 			
-		} catch (FileNotFoundException e) {
+		} catch (FileNotFoundException e) {		// 파일을 찾을 수 없는 경우
 			System.out.println("error: " + e);	
 			
-		} catch(IOException e){
+		} catch(IOException e){		// 파일을 읽을 때 또는 파일을 닫을 때 발생할 수 있는 일반적인 입출력 오류
 			System.out.println("error: " + e);
 		
-		} finally {
+		} finally {  // 무조건 실행
+			// 파일 정리
 			try {
 				if(fis != null) {
 					fis.close();
 				}
 			} catch(IOException e){
-				e.printStackTrace();
+				e.printStackTrace();  // 예외 정보 출력
 			
 			}
 		}
