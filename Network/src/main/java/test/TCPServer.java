6f72070edd061df9e500c9ea298876caea84c55c
BITCOM <BITCOM@BIT>
2024-05-01 14:45:00 +0900

    5/1 7교시 실습 (TCP Server)

--- Network/src/main/java/test/TCPServer.java
+++ Network/src/main/java/test/TCPServer.java
@@ -1,9 +1,12 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPServer {
 
@@ -15,17 +18,52 @@ public class TCPServer {
 			serverSocket = new ServerSocket();
 			
 			// 2. 바인딩 - 소켓의 주소(InetSocketAddress[InetAddress(IPAdress)+port])를 binding
-			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5000), 10);   // (IP, port), backlog
-			
-			// 특정 IP를 binding 하는 것은 좋지 않음
-			// IP 대역을 지정하지 않음 - 무조건 내 컴퓨터로 들어오게 끔 - port 번호만 일치 하다면
-			// IPAddress: 0.0.0.0: 특정 호스트 IP를 바인딩하지 않는다. 
-			// accept 처리 중에 다른 연결 요청이 들어올 경우를 대비한 공간(큐의 길이-10)
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", 5002), 10);   // (IP, port), backlog
 			
 			// 3. Accept
 			Socket socket = serverSocket.accept();  // blocking
-			
-			System.out.println("연결!!");   // 이 파일을 실행했을 땐 나오지 않고, 연결이 되면 바로 뜰 것 
+			try {
+				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();  // 다운 캐스팅 
+				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress(); // inetAddress 객체
+				int remotePort = inetRemoteSocketAddress.getPort();
+				System.out.println("[server] connected by client [" + remoteHostAddress + remotePort);
+				
+				// 4. IO Stream 받아오기
+				
+				InputStream is = socket.getInputStream();
+				OutputStream os = socket.getOutputStream();
+				
+				// 5. 데이터 읽기  - 기본적인 소켓 통신은 바이트 - 편의를 위해 데코레이터 사용(string으로 할 필요없으면 필요 X) 
+				while(true) {	// 서버는 무한 루프 
+					byte[] buffer = new byte[256];
+					int readByteCount = is.read(buffer);  //blocking
+				
+					if(readByteCount == -1) {   //  명시적으로 소켓 close()를 호출 - 클라이언트가 정상적인 종료
+						System.out.println("[server] closed by client");
+						break; // 더 읽을 것이 없으므로 반복문 종료
+					}
+					
+					// encoding
+					String data = new String(buffer, 0, readByteCount, "UTF-8"); 
+					System.out.println("[server] resived:" + data);
+				
+					
+				// 6. 데이터 쓰기
+					os.write(data.getBytes("utf-8"));
+				}
+				
+			} catch(SocketException e) {
+				System.out.println("[server] suddenly closed by client");
+			} catch(IOException e) {
+				System.out.println("[server] error:" + e);
+			}finally {
+				try {
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				
+				}
+			}
 			
 		} catch (IOException e) {
 			System.out.println("[server] error" + e);
